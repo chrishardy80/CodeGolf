d54811dd3399887a1853baebe3d640a733d8c89f
Adam Pridmore <adam_pridmore@hotmail.com>
2018-09-03 20:13:05 +0100

    - Added Fsharp solition

---
+++ Hole_1_FizzBuzz/FSharp/Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv =
+    [1..100] 
+    |> Seq.map(fun i -> 
+        match i%3=0, i%5=0 with
+        | true, true -> "FizzBuzz"
+        | true, false -> "Fizz"
+        | false, true -> "Buzz"
+        | _ -> i.ToString();
+    )
+    |> Seq.iter (printfn "%s")
+    0
