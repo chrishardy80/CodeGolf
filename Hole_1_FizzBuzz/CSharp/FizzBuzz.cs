ae3adb018a023e1aee42c3e19ada676481eb0ac9
Adam Pridmore <adam_pridmore@hotmail.com>
2018-09-03 19:55:02 +0100

    - Added csharp fizz buzz

--- Hole_1_FizzBuzz/CSharp/FizzBuzz.cs
+++ Hole_1_FizzBuzz/CSharp/FizzBuzz.cs
@@ -1,12 +1,19 @@
 ﻿using System;
-
-namespace CSharp
+class Program
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+static void Main(string[] a)
+{
+for(var i=0;i<101;++i){
+if(i%3==0){
+Console.Write("Fizz");
+}
+if(i%5==0){
+Console.Write("Buzz");
+}
+if(i%3!=0&&i%5!=0){
+Console.Write(i);
+}
+Console.WriteLine();
+}
+}
 }
