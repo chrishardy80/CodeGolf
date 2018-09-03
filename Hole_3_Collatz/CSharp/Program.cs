3b48e2ffc7d9839cfaa6ef73b68310dd750beb40
Adam Pridmore <adam_pridmore@hotmail.com>
2018-09-03 21:36:02 +0100

    - Added collatz solutions in FSharp and CSharp

---
+++ Hole_3_Collatz/CSharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Collatz
+{
+    class Program
+    {
+        static void Main(string[] a)
+        {
+            int i = 1;
+            for(var x = int.Parse(a[0]);x!=1;i++){
+                if (x%2==0){
+                    x = x / 2;
+                }else{
+                    x = (x * 3) + 1;
+                }
+            }
+            System.Console.WriteLine($"Length: {i}");
+        }
+    }
+}
---
+++ Hole_3_Collatz/FSharp/Program.fs
@@ -0,0 +1,15 @@
+﻿[<EntryPoint>]
+let main argv =
+  let a=System.Int32.Parse(argv.[0])
+  let next x=
+    match x with
+    | x when x%2=0->x/2
+    | _->x*3+1
+  let rec cseq n = 
+    match next n with
+    | n when n=1->[1]
+    | n -> [n]@cseq n
+  [a]@(cseq a)
+  |>Seq.length
+  |>printfn "%d"
+  0
