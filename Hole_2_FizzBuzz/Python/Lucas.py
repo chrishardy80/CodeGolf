359b5ab9712bf5dd6e569930db2bb76aac9f742d
Lucas Hermann <9932157+HermannLucas@users.noreply.github.com>
2018-09-11 18:57:07 +0100

    Update to start loop at 1

--- Hole_2_FizzBuzz/Python/Lucas.py
+++ Hole_2_FizzBuzz/Python/Lucas.py
@@ -1,3 +1,3 @@
 f="Fizz"
 b="Buzz"
-[print(l) for l in [f+b if n%15==0 else f if n%3==0 else b if n%5==0 else n for n in range(101)]]
+[print(l) for l in [f+b if n%15==0 else f if n%3==0 else b if n%5==0 else n for n in range(1,101)]]
